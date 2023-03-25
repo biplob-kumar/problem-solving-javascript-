c7b2589e98833b541fcaf1ba2f158c679315aa5b
biplob kumar <biplobkumar125@gmail.com>
2023-03-25 14:19:59 +0600

    problem-one

---
+++ problem1/index.js
@@ -0,0 +1,29 @@
+
+
+// Write a JavaScript Function   printNumber that takes an integer N and prints all the numbers between 0 to N.     ?????
+
+// ans  
+function printNumber() {
+    var n=parseFloat(prompt("Enter your number "))
+    for(var i=0; i<=n; i++){
+        document.write(i)
+      }
+    return n
+}
+printNumber()
+
+
+// 2 dye mentional array 
+
+
+
+
+
+
+
+
+
+
+
+
+
