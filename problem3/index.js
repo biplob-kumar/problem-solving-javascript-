fdc99858f54023b91f499a0948025e4bd0c8d62b
biplob kumar <biplobkumar125@gmail.com>
2023-03-27 17:44:14 +0600

    problem3

---
+++ problem3/index.js
@@ -0,0 +1,24 @@
+
+//  problem 3
+
+// Write a JS function AreaOfTringle that takes the base and height of a triangle and returns its area.????
+
+// ans:
+
+// Example 1
+var base=7;
+var hight=8;
+
+var area=(base*hight)/2
+
+console.log(area);
+
+// example 2
+// ans :
+
+var base=10;
+var hight=10;
+
+var area=(base*hight)/2
+
+console.log(area);
---
+++ problem4/index.js
@@ -0,0 +1,5 @@
+
+// Write a JavaScript Function that takes a word and returns the new word without including the first two characters.?????
+
+// ans ::
+
