86b348a3fa9b3960b8aac5cafeceb816590f5677
biplob kumar <biplobkumar125@gmail.com>
2023-03-29 20:52:04 +0600

    problem2

--- problem2/index.js
+++ problem2/index.js
@@ -3,9 +3,13 @@
 //   2 .Write a JavaScript function countWords  that takes a string and returns the number of words contained in this string. ??????
 
 // ans :
-var input = "Lorem ipsum dolor sit amet consectetur"
-var count =0;
-var wordcount= input.length
-console.log(wordcount);
+
+var sentence = "Lorem ipsum dolor sit amet consectetur"
+function countWords(str) {
+    const word = str.split(' ');
+  
+    return word.filter(word => word !== '').length;
+  }
+  console.log(countWords(sentence));   //out put is 6 (ans )
 
 
