fa0a9abc2a9c6a3ae5fc0a539a35c6ee579a5452
biplob kumar <biplobkumar125@gmail.com>
2023-03-29 21:35:43 +0600

    problem6

--- Problem6/index.js
+++ Problem6/index.js
@@ -27,11 +27,11 @@ var a =[ 3, 6, 5, 4, 8]
 
 function reverseArray(input) {
     
-    var ret = new Array;var ret =new Array;
+    var r = new Array;
 for (var i = input.length-1; i >= 0; i--){
-    ret.push(input[i]);
+      r.push(input[i]);
 }
-return ret
+return r
 }
  var b=reverseArray(a)
  console.log(b);  //output   [ 8, 4, 5, 6, 3 ]  (ans)
